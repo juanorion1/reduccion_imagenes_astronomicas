c73b859090d8f46902573b0ef2642f33884d8920
Juan David Rendon Luna <juanorion1@gmail.com>
2022-07-23 01:41:37 -0500

    Se agragan nuevas excepciones al gitignore. Hay modificaciones menores en Notebooks e imagen.py

--- imagen.py
+++ imagen.py
@@ -7,6 +7,30 @@ import matplotlib
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 
+drawing = False # true if mouse is pressed
+mode = True # if True, draw rectangle. Press 'm' to toggle to curve
+ix,iy = -1,-1
+
+ref_point = []
+def shape_selection(event, x, y, flags, param):
+    # grab references to the global variables
+    global ref_point, crop
+
+    # if the left mouse button was clicked, record the starting
+    # (x, y) coordinates and indicate that cropping is being performed
+    if event == cv.EVENT_LBUTTONDOWN:
+        ref_point = [(x, y)]
+
+    # check to see if the left mouse button was released
+    elif event == cv.EVENT_LBUTTONUP:
+        # record the ending (x, y) coordinates and indicate that
+        # the cropping operation is finished
+        ref_point.append((x, y))
+
+        # draw a rectangle around the region of interest
+        cv.rectangle(img, ref_point[0], ref_point[1], (255, 255, 0), 2)
+        cv.imshow(name, img)
+
 def click_event(event, x, y, flags, params):
  
     # checking for left mouse clicks
@@ -36,21 +60,41 @@ imagen = imagenes[3]
 
 
 with fits.open(imagen) as hdul:
-    info = hdul.info()
+    #info = hdul.info()
     img = hdul[1].data
     header = hdul[1].header
 
 w = WCS(header)
 
-img_ = img.astype(np.uint8)  ## Hay que considerar cambiar el format 
+img = img.astype(np.uint8)  ## Hay que considerar cambiar el format 
 #plt.imshow(img, cmap="rainbow")
 #plt.show()
 name = imagen.split("/")[-1]
-cv.namedWindow(name, cv.WINDOW_GUI_NORMAL)
-cv.imshow(name, cv.cvtColor(img_, cv.COLOR_GRAY2BGR))
+#cv.namedWindow(name, cv.WINDOW_GUI_NORMAL)
+#cv.imshow(name, cv.cvtColor(img_, cv.COLOR_GRAY2BGR))
 #cv.imshow(name, img)
-cv.setMouseCallback(name, click_event)
-key = cv.waitKey(0)
+#cv.setMouseCallback(name, draw_circle)
+#key = cv.waitKey(0)
 
-if key == ord("q"):
-    cv.destroyAllWindows()
+#if key == ord("q"):
+#    cv.destroyAllWindows()
+cv.namedWindow(name, cv.WINDOW_GUI_NORMAL)
+#img = np.zeros((512,512,3), np.uint8)
+clone = img.copy()
+cv.setMouseCallback(name,shape_selection)
+# keep looping until the 'q' key is pressed
+while True:
+    # display the image and wait for a keypress
+    cv.imshow(name, img)
+    key = cv.waitKey(1) & 0xFF
+
+    # press 'r' to reset the window
+    if key == ord("r"):
+        img = clone.copy()
+
+    # if the 'c' key is pressed, break from the loop
+    elif key == ord("q"):
+        break
+
+# close all open windows
+cv.destroyAllWindows() 
